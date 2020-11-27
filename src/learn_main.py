d8a48e0d279c1c93e77d2ef8fb269a5162b7078a
neuzen-ryuki <account@ryuki-fujita.xyz>
2020-11-27 09:24:33 +0900

    とりあえず学習部分完成

--- src/learn_main.py
+++ src/learn_main.py
@@ -1,4 +1,5 @@
 # sys
+import os
 
 # 3rd
 import numpy as np
@@ -7,59 +8,87 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 # ours
-from params.Params import *
+from params import Params as p
+
+
+def generate_feed() :
+    dir_components = os.listdir(p.FEED_DIR)
+    files = [f for f in dir_components if os.path.isfile(os.path.join(p.FEED_DIR, f))]
+
+    for epoch in range(p.EPOCH) :
+        for file_name in files :
+            feed = np.load(f"{p.FEED_DIR}{file_name}")
+            yield [feed["m"], feed["p"], feed["s"], feed["h"]], feed["y"]
 
 
 def create_model() :
     ## 数牌処理CNN
-    mps_inputs = keras.layers.Input(shape=(N_ROW_MPS, N_COL, N_PLANE_MPS), name="mps_input")
-    x = layers.Conv2D(N_CH_MPS, (3,2), activation="relu", name="mpsConv1")(mps_inputs)
+    mps_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.MPS_PLANE), name="mps_input")
+    x = layers.Conv2D(p.MPS_CH, (3,2), activation="relu", name="mpsConv1")(mps_inputs)
     x = layers.BatchNormalization(name="mpsConv1_BN")(x)
-    x = layers.Conv2D(N_CH_MPS, (3,2), activation="relu", name="mpsConv2")(x)
+    x = layers.Conv2D(p.MPS_CH, (3,2), activation="relu", name="mpsConv2")(x)
     x = layers.BatchNormalization(name="mpsConv2_BN")(x)
-    x = layers.Conv2D(N_CH_MPS, (3,2), activation="relu", name="mpsConv3")(x)
+    x = layers.Conv2D(p.MPS_CH, (3,2), activation="relu", name="mpsConv3")(x)
     x = layers.BatchNormalization(name="mpsConv3_BN")(x)
     mps_outputs = layers.Flatten(name="mps_Flatten")(x)
 
     mps_model = keras.Model(mps_inputs, mps_outputs)
-    m_inputs = keras.layers.Input(shape=(N_ROW_MPS, N_COL, N_PLANE_MPS), name="m_input")
-    p_inputs = keras.layers.Input(shape=(N_ROW_MPS, N_COL, N_PLANE_MPS), name="p_input")
-    s_inputs = keras.layers.Input(shape=(N_ROW_MPS, N_COL, N_PLANE_MPS), name="s_input")
+    m_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.MPS_PLANE), name="m_input")
+    p_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.MPS_PLANE), name="p_input")
+    s_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.MPS_PLANE), name="s_input")
 
     m_outputs = mps_model(m_inputs)
     p_outputs = mps_model(p_inputs)
     s_outputs = mps_model(s_inputs)
 
     ## 字牌処理CNN
-    char_inputs = keras.layers.Input(shape=(N_ROW_CHAR, N_COL, N_PLANE_CHAR), name="char_input")
-    x = layers.Conv2D(N_CH_CHAR, (3,2), activation="relu", name="charConv1")(char_inputs)
-    x = layers.BatchNormalization(name="charConv1_BN")(x)
-    x = layers.Conv2D(N_CH_CHAR, (3,2), activation="relu", name="charConv2")(x)
-    x = layers.BatchNormalization(name="charConv2_BN")(x)
-    x = layers.Conv2D(N_CH_CHAR, (3,2), activation="relu", name="charConv3")(x)
-    x = layers.BatchNormalization(name="charConv3_BN")(x)
-    char_outputs = layers.Flatten(name="char_Flatten")(x)
+    h_inputs = keras.layers.Input(shape=(p.HONOR_ROW, p.COL, p.HONOR_PLANE), name="honor_input")
+    x = layers.Conv2D(p.HONOR_CH, (3,2), activation="relu", name="honorConv1")(h_inputs)
+    x = layers.BatchNormalization(name="honorConv1_BN")(x)
+    x = layers.Conv2D(p.HONOR_CH, (3,2), activation="relu", name="honorConv2")(x)
+    x = layers.BatchNormalization(name="honorConv2_BN")(x)
+    x = layers.Conv2D(p.HONOR_CH, (3,2), activation="relu", name="honorConv3")(x)
+    x = layers.BatchNormalization(name="honorConv3_BN")(x)
+    h_outputs = layers.Flatten(name="honor_Flatten")(x)
 
     ## MLP
-    auxiliary_inputs = keras.layers.Input(shape=(N_AUX_INPUT, ), name="auxiliary_input")
-    dense_inputs = layers.concatenate([m_outputs, p_outputs, s_outputs, char_outputs, auxiliary_inputs])
-    x = layers.Dense(N_UNITS, activation="relu", name="MAIN_MLP1")(dense_inputs)
+    dense_inputs = layers.concatenate([m_outputs, p_outputs, s_outputs, h_outputs])
+    x = layers.Dense(p.UNITS, activation="relu", name="MAIN_MLP1")(dense_inputs)
     x = layers.BatchNormalization(name="MAIN_MLP1_BN")(x)
-    x = layers.Dense(N_UNITS, activation="relu", name="MAIN_MLP2")(x)
+    x = layers.Dense(p.UNITS, activation="relu", name="MAIN_MLP2")(x)
     x = layers.BatchNormalization(name="MAIN_MLP2_BN")(x)
-    x = layers.Dense(N_UNITS, activation="relu", name="MAIN_MLP3")(x)
-    x = layers.BatchNormalization(name="MAIN_MLP3_BN")(x)
-    x = layers.Dense(N_UNITS, activation="relu", name="MAIN_MLP4")(x)
-    x = layers.BatchNormalization(name="MAIN_MLP4_BN")(x)
-    x = layers.Dense(N_UNITS, activation="relu", name="MAIN_MLP5")(x)
-    dense_outputs = layers.BatchNormalization(name="MAIN_MLP5_BN")(x)
+    x = layers.Dense(p.UNITS, activation="relu", name="MAIN_MLP3")(x)
+    dense_outputs = layers.BatchNormalization(name="MAIN_MLP3_BN")(x)
 
     ## 出力
-    outputs = layers.Dense(N_OUTPUT, activation="softmax", name="main_output")(dense_outputs)
-    model = keras.Model(inputs=[m_inputs, p_inputs, s_inputs, char_inputs, auxiliary_inputs], outputs=outputs)
+    outputs = layers.Dense(p.OUTPUT, activation="softmax", name="main_output")(dense_outputs)
+    model = keras.Model(inputs=[m_inputs, p_inputs, s_inputs, h_inputs], outputs=outputs)
 
     return model
 
 
 if __name__ ==  "__main__" :
+    # create and setting up model
     model = create_model()
+    adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=adam,
+        metrics=["categorical_accuracy"])
+
+    # load data for validation
+    val = np.load(f"{p.VAL_DIR}val.npz")
+
+    # setting up learning records
+    fpath = p.SAVE_PATH + "weights.{epoch:02d}-{val_loss:.6f}.hdf5"
+    cbf1 = keras.callbacks.ModelCheckpoint(filepath=fpath, monitor="val_loss", mode="auto")
+    cbf2 = keras.callbacks.CSVLogger(p.SAVE_DIR + "result_history.csv")
+
+    # learning
+    model.fit(
+        generate_feed(),
+        steps_per_epoch=p.EVALUATE_SPAN,
+        validation_data=([val["m"], val["p"], val["s"], val["h"]], val["y"]),
+        epochs=int((p.FEED_FILES_NUM // p.VALIDATE_SPAN) * p.EPOCH),
+        verbose=1,
+        callbacks=[cbf1,cbf2])
--- src/params.py
+++ src/params.py
@@ -3,11 +3,26 @@ class ConstMeta(type):
         if name in self.__dict__: raise TypeError(f'Can\'t rebind const ({name})')
         else: self.__setattr__(name, value)
 
+
 class Params(metaclass=ConstMeta) :
-    BATCH_SIZE    = 5000
-    N_ROW_MPS     = 9
-    N_ROW_HONOR   = 7
-    N_COL         = 4
-    N_PLANE_MPS   = 2
-    N_PLANE_HONOR = 2
-    N_OUTPUT      = 38
+    FEED_FILES_NUM  = 10000
+    BATCH_SIZE      = 5000
+    VALIDATE_SPAN   = 2000
+    EPOCH           = 3
+
+    MPS_ROW         = 9
+    MPS_PLANE       = 2
+    MPS_CH          = 128
+
+    HONOR_ROW       = 7
+    HONOR_CH        = 128
+    HONOR_PLANE     = 2
+
+    UNITS           = 256
+
+    COL             = 4
+    OUTPUT          = 38
+
+    SAVE_DIR        = "../data/model/"
+    FEED_DIR        = "../data/feed/2019/"
+    VAL_DIR         = "../data/val/2019/"
