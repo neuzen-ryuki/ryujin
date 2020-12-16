e7c907252d2bf73df38fe33a7ee8191446a15fb9
neuzen-ryuki <account@ryuki-fujita.xyz>
2020-12-16 22:16:58 +0900

    とりあえず必要そうな物うめた

--- action.py
+++ action.py
@@ -1,4 +1,5 @@
 # sys
+from typing import List
 
 # 3rd
 import numpy as np
@@ -33,6 +34,7 @@ class Action :
 
 
     # 鳴くかどうか決める
+    # TODO 赤含みで鳴くかどうかまで決められてない
     def decide_to_steal(self, game, players, tile, pos, player_num) -> (int, int, int, int, int, int) :
         self.feed.write_feed_x(game, players, player_num)
         self.feed.write_about_steal_info(tile, pos)
@@ -46,6 +48,34 @@ class Action :
     # リーチするかどうか決める
     # TODO ちゃんと書く
     def decide_to_declare_ready(self, game, players, player_num) -> bool :
+        return True
+
+
+    # 槓するかどうか決める
+    # TODO ちゃんと書く
+    def decide_to_kan(self, game, players, player_num:int, ankan_tiles:Lits[int], kakan_tiles:List[int]) -> int :
+        return -1,
+
+
+    # 九種九牌を宣言するかどうか決める
+    # TODO ちゃんと書く
+    def decide_to_declare_nine_orphans(self, hand) -> bool :
+
+        # 九種九牌をそもそも宣言できるかどうかの判定
+        terminals_num = 0
+        for i in (TileType.TERMINALS | TileType.HONORS) :
+            if self.hand[i] > 0 :
+                terminals_num += 1
+
+        # 今はとりあえず流局できたらするようにする
+        if terminals_num > 8 : return True
+
+        return False
+
+
+    # 和了るかどうか決める
+    # TODO ちゃんと書く
+    def decide_win(self, game, players, player_num:int) -> True :
         return True
 
 
