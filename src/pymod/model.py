fa8d82e43d6b3e02c392c3c269b6f12bd76cc40b
neuzen-ryuki <account@ryuki-fujita.xyz>
2021-03-02 03:11:36 +0900

    upd: モデル生成モジュールの処理を細かく切り分けた

--- src/pymod/model.py
+++ src/pymod/model.py
@@ -10,9 +10,8 @@ from termcolor import colored
 from .params import Params as p
 
 
-# Neural Networkモデルを構築
-def create_model(mode:str) -> keras.Model :
-    ## 数牌処理CNN
+# 数牌処理CNNを構築
+def create_mps_network() :
     mps_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.PLANE), name="mps_input")
     x = layers.Conv2D(p.MPS_CH, (3,2), activation="relu", name="mpsConv1")(mps_inputs)
     x = layers.BatchNormalization(name="mpsConv1_BN")(x)
@@ -23,15 +22,19 @@ def create_model(mode:str) -> keras.Model :
     mps_outputs = layers.Flatten(name="mps_Flatten")(x)
 
     mps_model = keras.Model(mps_inputs, mps_outputs)
-    m_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.PLANE), name="m_input")
-    p_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.PLANE), name="p_input")
-    s_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.PLANE), name="s_input")
+    m_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.PLANE), name="m_inputs")
+    p_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.PLANE), name="p_inputs")
+    s_inputs = keras.layers.Input(shape=(p.MPS_ROW, p.COL, p.PLANE), name="s_inputs")
 
     m_outputs = mps_model(m_inputs)
     p_outputs = mps_model(p_inputs)
     s_outputs = mps_model(s_inputs)
 
-    ## 字牌処理CNN
+    return m_inputs, p_inputs, s_inputs, m_outputs, p_outputs, s_outputs
+
+
+# 字牌処理CNNを構築
+def create_honor_network() :
     h_inputs = keras.layers.Input(shape=(p.HONOR_ROW, p.COL, p.PLANE), name="honor_input")
     x = layers.Conv2D(p.HONOR_CH, (3,2), activation="relu", name="honorConv1")(h_inputs)
     x = layers.BatchNormalization(name="honorConv1_BN")(x)
@@ -41,7 +44,13 @@ def create_model(mode:str) -> keras.Model :
     x = layers.BatchNormalization(name="honorConv3_BN")(x)
     h_outputs = layers.Flatten(name="honor_Flatten")(x)
 
-    ## COMMON_MLP
+    return h_inputs, h_outputs
+
+
+# 各モデル共通部分のnetworkを構築
+def create_common_network() :
+    m_inputs, p_inputs, s_inputs, m_outputs, p_outputs, s_outputs = create_mps_network()
+    h_inputs, h_outputs = create_mps_network()
     aux_inputs = keras.layers.Input(shape=(p.AUX_INPUT, ), name="aux_input")
     common_inputs = layers.concatenate([m_outputs, p_outputs, s_outputs, h_outputs, aux_inputs])
     x = layers.Dense(p.COMMON_UNITS1, activation="relu", name="COMMON_MLP")(common_inputs)
@@ -49,14 +58,25 @@ def create_model(mode:str) -> keras.Model :
     x = layers.Dense(p.COMMON_UNITS2, activation="relu", name="COMMON_OUT")(x)
     common_outputs = layers.BatchNormalization(name="COMMON_OUT_BN")(x)
 
-    ## MAIN_MLP
+    return m_inputs, p_inputs, s_inputs, h_inputs, aux_inputs, common_outputs
+
+
+# 打牌判断モデルを構築
+def create_main_model() -> keras.Model :
+    m_inputs, p_inputs, s_inputs, h_inputs, aux_inputs, common_outputs = create_common_network()
     main_inputs = layers.Dense(p.UNITS, activation="relu", name="MAIN_MLP")(common_outputs)
     x = layers.BatchNormalization(name="MAIN_BN")(main_inputs)
     x = layers.Dense(p.UNITS, activation="relu", name="MAIN_OUT")(x)
     x = layers.BatchNormalization(name="MAIN_OUT_BN")(x)
     main_outputs = layers.Dense(p.MAIN_OUTPUT, activation="softmax", name="main_output")(x)
 
-    ## STEAL_MLP
+    model = keras.Model(inputs=[m_inputs, p_inputs, s_inputs, h_inputs, aux_inputs], outputs=main_outputs)
+    return model
+
+
+# 副露判断モデルを構築
+def create_steal_model() -> keras.Model :
+    m_inputs, p_inputs, s_inputs, h_inputs, aux_inputs, common_outputs = create_common_network()
     steal_info = keras.layers.Input(shape=(p.SI_INPUT, ), name="steal_tile")
     steal_inputs = layers.concatenate([common_outputs, steal_info])
     x = layers.Dense(p.UNITS, activation="relu", name="STEAL_MLP")(steal_inputs)
@@ -65,21 +85,31 @@ def create_model(mode:str) -> keras.Model :
     x = layers.BatchNormalization(name="STEAL_OUT_BN")(x)
     steal_outputs = layers.Dense(p.STEAL_OUTPUT, activation="softmax", name="steal_output")(x)
 
-    ## READY_MLP
+    model = keras.Model(inputs=[m_inputs, p_inputs, s_inputs, h_inputs, aux_inputs], outputs=steal_outputs)
+    return model
+
+
+# 副露判断モデルを構築
+def create_ready_model() -> keras.Model :
+    m_inputs, p_inputs, s_inputs, h_inputs, aux_inputs, common_outputs = create_common_network()
     ready_inputs = layers.Dense(p.UNITS, activation="relu", name="READY_MLP")(common_outputs)
     x = layers.BatchNormalization(name="READY_BN")(ready_inputs)
     x = layers.Dense(p.UNITS, activation="relu", name="READY_OUT")(x)
     x = layers.BatchNormalization(name="READY_OUT_BN")(x)
     ready_outputs = layers.Dense(p.READY_OUTPUT, activation="softmax", name="ready_output")(x)
 
-    ## create_model
-    model = keras.Model(inputs=[m_inputs, p_inputs, s_inputs, h_inputs, steal_info, aux_inputs],
-                        outputs=[main_outputs, steal_outputs, ready_outputs])
+    model = keras.Model(inputs=[m_inputs, p_inputs, s_inputs, h_inputs, aux_inputs], outputs=ready_outputs)
+    return model
 
-    ## モデルの形を出力
+
+# modeに対応するモデルを構築
+def create_model(mode:str) -> keras.Model :
+    if   mode == "main"  : model = create_main_model()
+    elif mode == "steal" : model = create_steal_model()
+    elif mode == "ready" : model = create_ready_model()
+
     keras.utils.plot_model(model, f"{p.RESULT_DIR}/ryujin_model.png", show_shapes=True)
 
-    ## setting up model
     model.compile(
         optimizer="adam",
         loss="categorical_crossentropy",
